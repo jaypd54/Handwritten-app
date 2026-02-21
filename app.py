b175e7e4e7de4cc17e59945d78515216101a3b40
jaypd54 <jaypd54@gmail.com>
2026-02-21 21:59:41 +0800

    Update import statement for Keras model loading

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 from preprocess import preprocess_image
